f604bf470ebe1eadcd90c419801c5a82377b2f49
Alex White <alex@awsoftware.co.uk>
2023-04-11 13:28:18 +0100

    bugfixes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { Client, Events, IntentsBitField } = require("discord.js");
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv/config");
 
+let allowedChannels = ["1095313352596668428"];
+
+const testingAllowedChannels = ["1095307902413189141"];
+allowedChannels = testingAllowedChannels;
 const discordClient = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -17,70 +21,71 @@ const openApiClientConfig = new Configuration({
 const openAiClient = new OpenAIApi(openApiClientConfig);
 
 discordClient.on("messageCreate", async (msg) => {
-  if (msg.author.bot) return;
-  if (msg.content.startsWith("!")) return;
+  try {
+    if (allowedChannels.indexOf(msg.channelId) == -1) return;
+    if (msg.author.bot) return;
+    if (msg.content.startsWith("!")) return;
 
-  const conversationLog = [
-    {
-      role: "system",
-      content:
-        "You are a friendly chatbot but when provoked will happily take the piss out of people.",
-    },
-  ];
+    const conversationLog = [
+      {
+        role: "system",
+        content: "You are a friendly chatbot.",
+      },
+    ];
 
-  await msg.channel.sendTyping();
+    await msg.channel.sendTyping();
 
-  const channelMessageHistory = await msg.channel.messages.fetch({ limit: 50 });
-  channelMessageHistory.reverse();
+    const channelMessageHistory = await msg.channel.messages.fetch({
+      limit: 50,
+    });
+    channelMessageHistory.reverse();
 
-  for (const messageHistoryItem of channelMessageHistory) {
-    const historicalMessage = messageHistoryItem[1];
+    for (const messageHistoryItem of channelMessageHistory) {
+      const historicalMessage = messageHistoryItem[1];
 
-    if (
-      !historicalMessage.content ||
-      historicalMessage.content.startsWith("!") ||
-      (historicalMessage.author.bot &&
-        historicalMessage.author.id != discordClient.user.id)
-    ) {
-      continue;
-    }
+      if (
+        !historicalMessage.content ||
+        historicalMessage.content.startsWith("!") ||
+        (historicalMessage.author.bot &&
+          historicalMessage.author.id != discordClient.user.id)
+      ) {
+        continue;
+      }
 
-    if (historicalMessage.author.id === msg.author.id) {
-      conversationLog.push({
-        role: "user",
-        content: historicalMessage.content,
-      });
-    } else if (historicalMessage.author.id === discordClient.user.id) {
-      if (historicalMessage.content.startsWith("ERROR:")) continue;
-      //if message was sent by gpt-bot, check that the message it was replying to is from the same user as current message
-      if (historicalMessage.messageReference) {
-        const originalMessage = await message.channel.messages.fetch(
-          historicalMessage.messageReference
-        );
-        if (originalMessage.author.id === message.author.id) {
-          conversationLog.push({
-            role: "system",
-            content: historicalMessage.content,
-          });
+      if (historicalMessage.author.id === msg.author.id) {
+        conversationLog.push({
+          role: "user",
+          content: historicalMessage.content,
+        });
+      } else if (historicalMessage.author.id === discordClient.user.id) {
+        if (historicalMessage.content.startsWith("ERROR:")) continue;
+        //if message was sent by gpt-bot, check that the message it was replying to is from the same user as current message
+        if (historicalMessage.reference) {
+          const originalMessage = await historicalMessage.fetchReference();
+          if (originalMessage.author.id === msg.author.id) {
+            conversationLog.push({
+              role: "assistant",
+              content: historicalMessage.content,
+            });
+          }
         }
       }
-      conversationLog.push({
-        role: "system",
-        content: historicalMessage.content,
+    }
+
+    try {
+      const result = await openAiClient.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: conversationLog,
+      });
+      msg.reply(result.data.choices[0].message);
+    } catch (err) {
+      msg.reply({
+        content: "ERROR: " + error.message,
       });
     }
-  }
-
-  try {
-    const result = await openAiClient.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: conversationLog,
-    });
-    msg.reply(result.data.choices[0].message);
-  } catch (err) {
-    msg.reply({
-      content: "ERROR: " + error.message,
-    });
+  } catch (error) {
+    msg.reply("ERROR: " + JSON.stringify(error));
+    console.log(error);
   }
 });
 
